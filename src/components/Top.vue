cfc27498297650c40755cd7ff654ad378c3e558e
Jeffry <jeffry0857@gmail.com>
2022-12-01 22:44:26 +0800

    added top component

---
+++ src/components/Top.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex justify-between">
+    <input type="text" placeholder="Enter your itinerary" v-model="title">
+    <button @click="add()">Add</button>
+  </div>
+</template>
+
+<script>
+import { nanoid } from 'nanoid'
+export default {
+  name: 'Top',
+  props: ['addTodo'],
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    add() {
+      if (!this.title.trim()) return alert('cannot be empty')
+      const todoObj = {id: nanoid(), title: this.title, done: false}
+      this.addTodo(todoObj)
+      this.title = ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
