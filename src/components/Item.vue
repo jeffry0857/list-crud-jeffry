89ef39a33c3c037803e606d9cec74231db1cd0c3
Jeffry <jeffry0857@gmail.com>
2022-12-01 16:04:19 +0800

    Added List and Item component

---
+++ src/components/Item.vue
@@ -0,0 +1,32 @@
+<template>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+  <li>
+      <label>
+        <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)" />
+        <span>{{ todo.title }}</span>
+      </label>
+      <button class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red" @click="handleDelete(todo.id)">Delete</button>
+    </li>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: ['todo', 'checkTodo', 'deleteTodo'],
+  methods: {
+    handleCheck(id) {
+      this.checkTodo(id)
+    },
+    handleDelete(id) {
+      if (confirm('Sure to delete ?'))
+      {
+        this.deleteTodo(id)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="todo-main">
+    <Item 
+      v-for="todoObj in todos" 
+      :key="todoObj.id" 
+      :todo="todoObj" 
+      :checkTodo="checkTodo" 
+      :deleteTodo="deleteTodo" />
+  </ul>
+</template>
+
+<script>
+import Item from './Item.vue';
+export default {
+  components: { Item },
+  props: ['todos', 'checkTodo', 'deleteTodo']
+}
+</script>
+
+<style scoped>
+  .todo-main {
+    margin-left: 0;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0;
+  }
+</style>
